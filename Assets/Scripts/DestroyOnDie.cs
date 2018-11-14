04a40d52fe8a5278abc9d3ff99df4ac9b986cde5
Artizazel <samuelbloxham@hotmail.co.uk>
2018-11-14 01:08:35 +0000

    Completed Menu Transition

---
+++ Assets/Scripts/DestroyOnDie.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnDie : MonoBehaviour {
+
+	
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/scrMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scrMainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Act1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+	
+
+
+}
